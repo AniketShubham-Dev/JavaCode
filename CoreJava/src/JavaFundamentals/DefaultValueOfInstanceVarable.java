3402c90d7b13007693f481bac7f96604a79903d4
AniketShubham <aniketshubham99@gmail.com>
2022-10-06 01:01:26 +0530

    Default value of Instance variable

--- CoreJava/src/JavaFundamentals/DefaultValueOfInstanceVarable.java
+++ CoreJava/src/JavaFundamentals/DefaultValueOfInstanceVarable.java
@@ -1,6 +1,7 @@
 package JavaFundamentals;
 
 public class DefaultValueOfInstanceVarable {
+	//Creating Instance Variable without assigning values
 	byte b;
 	short s;
 	int i;
@@ -13,14 +14,15 @@ public class DefaultValueOfInstanceVarable {
 	
 public static void main(String[] args) {
 	DefaultValueOfInstanceVarable obj = new DefaultValueOfInstanceVarable();
-	System.out.println(obj.b);
-	System.out.println(obj.s);
-	System.out.println(obj.i);
-	System.out.println(obj.l);
-	System.out.println(obj.f);
-	System.out.println(obj.d);
-	System.out.println(obj.c);
-	System.out.println(obj.bol);
-	System.out.println(obj.str);
+	//Printing Instance Variables
+	System.out.println("Default value of byte data type = "+obj.b);
+	System.out.println("Default value of short data type = "+obj.s);
+	System.out.println("Default value of int data type = "+obj.i);
+	System.out.println("Default value of long data type = "+obj.l);
+	System.out.println("Default value of float data type = "+obj.f);
+	System.out.println("Default value of double data type = "+obj.d);
+	System.out.println("Default value of char data type = "+obj.c);
+	System.out.println("Default value of boolean data type = "+obj.bol);
+	System.out.println("Default value of String data type = "+obj.str);
 }
 }
