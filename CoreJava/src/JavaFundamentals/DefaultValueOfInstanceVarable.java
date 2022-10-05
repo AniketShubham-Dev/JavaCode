0c3ed4de23f3e005e0ad465ef17ba252510ba871
AniketShubham <aniketshubham99@gmail.com>
2022-10-06 00:53:12 +0530

    Program to show default value of Instance Variable

---
+++ CoreJava/src/JavaFundamentals/DefaultValueOfInstanceVarable.java
@@ -0,0 +1,26 @@
+package JavaFundamentals;
+
+public class DefaultValueOfInstanceVarable {
+	byte b;
+	short s;
+	int i;
+	long l;
+	float f;
+	double d;
+	char c;
+	boolean bol;
+	String str;
+	
+public static void main(String[] args) {
+	DefaultValueOfInstanceVarable obj = new DefaultValueOfInstanceVarable();
+	System.out.println(obj.b);
+	System.out.println(obj.s);
+	System.out.println(obj.i);
+	System.out.println(obj.l);
+	System.out.println(obj.f);
+	System.out.println(obj.d);
+	System.out.println(obj.c);
+	System.out.println(obj.bol);
+	System.out.println(obj.str);
+}
+}
